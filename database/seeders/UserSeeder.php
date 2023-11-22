1acc9f01e8564cfa880ff676372a1581f0913509
restu aditya setyawan <restuadityasetyawan@gmail.com>
2023-11-22 21:37:49 +0700

    laravel, fature users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::create([
+            'name' => 'Admin Adit',
+            'email' => 'adit@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'roles' => 'Admin',
+            'phone' => '0895326767872',
+        ]);
+    }
+}
---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Adit Stywn
+  </div>

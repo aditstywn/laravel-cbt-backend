31c1c37a9d302408b379adbebe54c5913cc27ded
restu aditya setyawan <restuadityasetyawan@gmail.com>
2023-12-13 21:48:14 +0700

    add status ujian, timer

--- app/Http/Controllers/Api/UjianController.php
+++ app/Http/Controllers/Api/UjianController.php
@@ -116,10 +116,18 @@ class UjianController extends Controller
 
         $soal = Soal::whereIn('id', $ujianSoalListId)->where('kategori', $request->kategori)->get();
 
-
+        //  timer by kategori
+        $timer = $ujian->timer_angka;
+        if ($request->kategori == 'verbal') {
+            $timer = $ujian->timer_verbal;
+        } else if ($request->kategori == 'logika') {
+            $timer = $ujian->timer_logika;
+        }
 
         return response()->json([
+
             'message' => 'Berhasil Mendapatkan soal',
+            'timer' => $timer,
             'data' => SoalResource::collection($soal),
         ]);
     }
